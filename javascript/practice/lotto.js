2df803321fabb5aa93dc03e7ef2bcf382c8be369
jackiehoon <wognsdl4545@naver.com>
2022-02-21 13:15:10 +0900

    day:13 타이머

--- javascript/practice/lotto.js
+++ javascript/practice/lotto.js
@@ -58,7 +58,56 @@ const recommend = () => {
     $lottoNumberList[i].classList.add(colorClass);
   }
 };
-// 버튼 누를때마다 번호 추천해주기
-document.querySelector("#btn-start").addEventListener("click", recommend);
 
-recommend();
+const printBall = (target, number) => {
+  target.innerText = number;
+  // numberList[i] 값에 따라서 class 붙이기
+  let colorClass = "";
+  if (number <= 10) {
+    colorClass = "yellow";
+  } else if (number <= 20) {
+    colorClass = "blue";
+  } else if (number <= 30) {
+    colorClass = "red";
+  } else if (number <= 40) {
+    colorClass = "gray";
+  } else {
+    colorClass = "green";
+  }
+  target.classList = "lotto-number";
+  target.classList.add(colorClass);
+};
+
+const recommend2 = () => {
+  //  중복없는, 정렬된 배열 만들기
+  const numberList = [];
+  for (let i = 0; i < 6; i++) {
+    let randomNumber = getRandomNumber();
+    while (numberList.includes(randomNumber)) {
+      randomNumber = getRandomNumber();
+    }
+    numberList.push(randomNumber);
+  }
+  numberList.sort((a, b) => a - b);
+
+  const $lottoNumberList = document.querySelectorAll(".lotto-number");
+  for (let i = 0; i < $lottoNumberList.length; i++) {
+    // 0.1초마다 랜덤숫자 출력
+    const intervalId = setInterval(() => {
+      const number = getRandomNumber();
+      printBall($lottoNumberList[i], number);
+    }, 100);
+
+    // i*1초마다 배열의 숫자 출력
+    setTimeout(() => {
+      // 0.1초 랜덤출력 멈추기
+      clearInterval(intervalId);
+      printBall($lottoNumberList[i], numberList[i]);
+    }, i * 1000);
+  }
+};
+
+// 버튼 누를때마다 번호 추천해주기
+document.querySelector("#btn-start").addEventListener("click", recommend2);
+
+// recommend();
